9338f9af7263bf4c2e419df1eac3f5277b0f59c4
devLucasYuji <sidnei@sidneisecurityservice.com.br>
2020-08-16 04:17:16 -0300

    fixed percent from accessibility, SEO and best practices

--- assets/js/main.min-debug.js
+++ assets/js/main.min-debug.js
@@ -38,6 +38,8 @@
 
     var $btnUp = document.querySelector("#up");
     var $btnHomepage = document.querySelector("#homepageButton");
+    var $logo = document.querySelector("#logo");
+
     var $sections = {
         about: document.querySelector("#about"),
         partner: document.querySelector("#partner"),
@@ -45,7 +47,9 @@
         contact: document.querySelector("#contact")
     }    
 
+
     $btnHomepage.addEventListener("click", () => scrollToElement($sections.about));
+    $logo.addEventListener("click", () => scrollToElement(document.querySelector('body')));
     $btnUp.addEventListener("click", () => scrollToElement(document.querySelector('body')));
     document.addEventListener('scroll', menu);
 })();
