a60c8c7736a29c848c93067506608768d2d7a6ec
devLucasYuji <lucas.yuji@madeinweb.com.br>
2020-08-16 03:54:10 -0300

    adjust about us section

--- assets/js/main.min-debug.js
+++ assets/js/main.min-debug.js
@@ -45,13 +45,6 @@
         contact: document.querySelector("#contact")
     }    
 
-    function scrollSections(button){
-        document.querySelector("#" + button).addEventListener("click", () => scrollToElement($sections[button]), false);
-    }
-
-    Object.keys($sections).forEach(function(e){
-        scrollSections(e);
-    });
     $btnHomepage.addEventListener("click", () => scrollToElement($sections.about));
     $btnUp.addEventListener("click", () => scrollToElement(document.querySelector('body')));
     document.addEventListener('scroll', menu);
